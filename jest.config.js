b65d9bdbd73b9196003138297a2ef8b8044213fc
Zeindelf <zeindelf@hotmail.com>
2020-01-01 05:02:32 -0300

    test: fix exactly src directories to tests

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,10 @@
 module.exports = {
+  preset: 'ts-jest',
+  coverageDirectory: 'coverage',
   verbose: true,
   testMatch: [
-    '<rootDir>/__tests__/**/*.+(ts|tsx|js)',
-    '<rootDir>/src/**/?(*.)+(spec|test).+(ts|tsx|js)',
+    '<rootDir>/tests/**/*.+(js|ts)',
+    '<rootDir>/src/**/?(*.)+(spec|test).+(js|ts)',
   ],
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest',
